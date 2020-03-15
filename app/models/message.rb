c16cb929ee172bf661565d5defdfacfaecf69294
MTKS-3254-KNT <s.mitsukoshi@gmail.com>
2020-03-15 18:04:18 +0900

    TestChord実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,9 @@
 class Message < ApplicationRecord
+  mount_uploader :image, ImageUploader
+  
   validates :text, presence: true, unless: :image?
 
   belongs_to :user
   belongs_to :group
 
-  mount_uploader :image, ImageUploader
 end
