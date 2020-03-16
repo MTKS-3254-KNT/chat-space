ab98be49ee06dfccdbe9377b3801e260b9be84de
MTKS-3254-KNT <s.mitsukoshi@gmail.com>
2020-03-16 20:32:08 +0900

    インクリメンタルサーチ実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ApplicationRecord
   has_many :messages
   has_many :group_users
   has_many :groups, through: :group_users
+
+  def self.search(input, id)
+    # クラスメソッドの定義、引数は検索ワードと自身のID
+    return nil if input == ""
+    # ワードがなければnilを返す、キーワドが空白で無い場合は⬇へ
+    User.where(['name LIKE ?', "%#{input}%"] ).where.not(id: id).limit(10)
+    # キーワードを元にユーザーを曖昧検索、自身のIDは除外、検索するのは10件だけ
+  end
+  # 検索したワードを含むレコード、もしくはnilを返す
 end
